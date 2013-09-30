b82df65aacfa0e026096678371074735b55d3b97
jmindek <jerry.mindek@gmail.com>
2013-09-30 17:40:52 -0400

    WIP: Getting the handler to return 400 for an invalid IP.

--- src/geo_ip_api/handler.clj
+++ src/geo_ip_api/handler.clj
@@ -2,15 +2,22 @@
   (:use compojure.core)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]))
+
+(defn parse-int [s]
+  (Integer. (re-find #"[0-9]+" s)))
+
 (defn verify-ip [ip]
   (pos? (count (re-seq #"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}" ip))))
 
 (defn fetch-city-state [ip]
-  (verify-ip ip))
+  (let [good-ip (verify-ip ip)]
+    (cond 
+          (true? good-ip) 200
+          :else (parse-int "400"))))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
-  (GET "/city-state/:ip" [ip] (str (fetch-city-state ip)))
+  (GET "/city-state/:ip" [ip] (fetch-city-state ip))
   (GET "/documentation" [] "<h1>Documentation</h1><p>None yet</p>")
   (GET "/health-check" [] "<h1>Health Check</h1><p>TBD</p>")
   (route/resources "/")
@@ -18,5 +25,3 @@
 
 (def app
   (handler/site app-routes))
-
-
