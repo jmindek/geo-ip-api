a58bf49159786a4393173884a9f88c9ef456adef
jmindek <jerry.mindek@gmail.com>
2013-10-04 13:53:12 -0400

    Added ring to list of dependencies to see if that eliminates the new error at Travis complaining about not finding ring/mock/...

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Simple ip to geo web api."
   :url "http://github.com/jmindek/geo-ip-api.git"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]]
+                 [compojure "1.1.5"]
+                 [ring "1.2.0"]]
   :plugins [[lein-ring "0.8.5"],[lein-ring "0.8.7"]]
   :ring {:handler geo-ip-api.handler/app}
   :profiles
