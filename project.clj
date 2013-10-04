a0799ab8d12a07d0f4ce04666796db25b006f9e6
jmindek <jerry.mindek@gmail.com>
2013-10-04 14:00:07 -0400

    Updated lein-ring version to 0.8.7

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
                  [ring "1.2.0"]]
-  :plugins [[lein-ring "0.8.5"],[lein-ring "0.8.7"]]
+  :plugins [[lein-ring "0.8.7"],[lein-ring "0.8.7"]]
   :ring {:handler geo-ip-api.handler/app}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.5"]]}})
